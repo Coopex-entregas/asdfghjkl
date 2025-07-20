b398bd566b916f14fbcb580367e71aa8cc012876
Coopex-entregas <coopexentregas.rn@gmail.com>
2025-07-20 15:45:53 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -7,21 +7,19 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), unique=True, nullable=False)
     senha = db.Column(db.String(255), nullable=False)
-    tipo = db.Column(db.String(50), default='admin')  # 'admin' ou 'cooperado'
+    tipo = db.Column(db.String(20), nullable=False)  # 'admin' ou 'cooperado'
 
 class Cooperado(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=True)
-    telefone = db.Column(db.String(20), nullable=True)
+    nome = db.Column(db.String(100), unique=True, nullable=False)
 
 class Entrega(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cliente = db.Column(db.String(100), nullable=False)
-    bairro = db.Column(db.String(100), nullable=True)
-    valor = db.Column(db.Float, nullable=True)
-    status = db.Column(db.String(20), default='Pendente')
+    bairro = db.Column(db.String(100))
+    valor = db.Column(db.Float)
+    status = db.Column(db.String(20))
     cooperado_id = db.Column(db.Integer, db.ForeignKey('cooperado.id'), nullable=True)
-    cooperado_nome = db.Column(db.String(100), nullable=True)
+    cooperado = db.relationship('Cooperado', backref='entregas')
     data_criacao = db.Column(db.DateTime, default=datetime.utcnow)
-    data_entrega = db.Column(db.DateTime, nullable=True)  # data/hora recebida pelo motoboy
+    data_recebimento = db.Column(db.DateTime, nullable=True)
