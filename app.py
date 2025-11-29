4e72b4083951e8e38732f0b1a778e6791f499660
Coopex-entregas <coopexentregas.rn@gmail.com>
2025-11-29 16:46:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6066,7 +6066,6 @@ def handle_atualizar_entrega(data):
     )
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 
