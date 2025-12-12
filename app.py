2eaad52a71d410b413e2f600b1c9aeeb7f28fd57
Coopex-entregas <coopexentregas.rn@gmail.com>
2025-12-12 18:45:34 -0300

    Update SocketIO configuration for eventlet support

--- app.py
+++ app.py
@@ -41,11 +41,16 @@ app.config['SECRET_KEY'] = os.environ.get(
 )
 
 # 🔽 INSTÂNCIA DO SOCKETIO LIGADA NO APP
+# IMPORTANTE: eventlet tem que vir ANTES de importar coisas que usam rede/threads
+import eventlet
+eventlet.monkey_patch()
+
 from flask_socketio import SocketIO
 
 socketio = SocketIO(
     app,
-    async_mode="threading",   # (opcional, mas bom deixar explícito)
+    cors_allowed_origins="*",
+    async_mode="eventlet",
     logger=False,
     engineio_logger=False
 )
